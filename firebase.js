e04b56f7ad17f9cd983cf1905c752f58288af74d
kaffahstorage-stack <kaffahstorage@gmail.com>
2026-06-07 11:15:35 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./marketplacewira-firebase-adminsdk-fbsvc-ec307a8f4f.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://marketplacewira-default-rtdb.asia-southeast1.firebasedatabase.app"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 const db = admin.database();
 
 module.exports = db;
-console.log("PROJECT ID:", serviceAccount.project_id);
-console.log("CLIENT EMAIL:", serviceAccount.client_email);
